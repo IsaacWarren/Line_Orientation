6b26d3a0e3fffa22c3f36eedb3ca242684f63df1
Isaac <iwarren2@illinois.edu>
2020-12-28 11:45:55 -0600

    Add libraries

---
+++ letter.js
@@ -0,0 +1,14 @@
+export default class Letter {
+    constructor(x, y, char) {
+        this.x = x;
+        this.y = y;
+        this.char = char;
+        this.dragging = false;
+        this.locked = false;
+    }
+    setDim(width, height){
+        this.width = width;
+        this.height = height;
+    }
+}
+
